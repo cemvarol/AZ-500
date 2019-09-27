d5a0461f4a167eacc0f9a78f7ca8434645144a08
cemvarol <cem.varol@outlook.com>
2019-09-27 14:47:47 +0100

    Create PromoteDC.ps1

---
+++ PromoteDC.ps1
@@ -0,0 +1,16 @@
+install-windowsfeature AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDeployment
+$dsrmPassword = (ConvertTo-SecureString -AsPlainText -Force -String "Potato2019")
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "aurian.com" `
+-DomainNetbiosName "AURIAN" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-SafeModeAdministratorPassword $dsrmPassword `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
